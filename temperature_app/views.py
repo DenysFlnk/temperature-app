1a61ee6d38d18cfcbdcd0581253f3c05d8e003e0
DenysFlnk <filonenko.denys94@gmail.com>
2024-10-15 09:00:52 +0300

    add air quality info to response

--- temperature_app/views.py
+++ temperature_app/views.py
@@ -11,22 +11,44 @@ from temperature_app.models import Worldcities
 TEMPERATURE_API_ENDPOINT = 'https://api.open-meteo.com/v1/forecast'
 AIR_QUALITY_API_ENDPOINT = 'https://air-quality-api.open-meteo.com/v1/air-quality'
 
+POLLUTANT_DICT = {20: 'Good',
+                  40: 'Fair',
+                  60: 'Moderate',
+                  80: 'Poor',
+                  100: 'Very Poor',}
+
 
 def temperature_here(request):
     location: list[str] = geocoder.ip('me').latlng
-    temperature = get_temperature(location[0], location[1])
-
     world_city = get_city(float(location[0]), float(location[1]))
-    context = {'country': world_city.country, 'city': world_city.city, 'temperature': temperature}
+
+    temperature = get_temperature(location[0], location[1])
+    air_quality_index = get_air_quality_index(location[0], location[1])
+    air_quality_definition = get_air_quality_definition(air_quality_index)
+
+    context = {'country': world_city.country,
+               'city': world_city.city,
+               'temperature': temperature,
+               'air_quality_index': air_quality_index,
+               'air_quality_definition': air_quality_definition}
+
     template = loader.get_template('index.html')
     return HttpResponse(template.render(context, request))
 
 
 def temperature_random(request):
     random_city = Worldcities.objects.order_by('?').first()
-    temperature = get_temperature(random_city.lat, random_city.lng)
 
-    context = {'country': random_city.country, 'city': random_city.city, 'temperature': temperature}
+    temperature = get_temperature(random_city.lat, random_city.lng)
+    air_quality_index = get_air_quality_index(random_city.lat, random_city.lng)
+    air_quality_definition = get_air_quality_definition(air_quality_index)
+
+    context = {'country': random_city.country,
+               'city': random_city.city,
+               'temperature': temperature,
+               'air_quality_index': air_quality_index,
+               'air_quality_definition': air_quality_definition}
+
     template = loader.get_template('index.html')
     return HttpResponse(template.render(context, request))
 
@@ -39,12 +61,20 @@ def get_temperature(lat: str, lng: str) -> str:
     return meteo_data['hourly']['temperature_2m'][hour]
 
 
-def get_air_quality(lat: str, lng: str) -> str:
+def get_air_quality_index(lat: str, lng: str) -> int:
     api_request = f'{AIR_QUALITY_API_ENDPOINT}?latitude={lat}&longitude={lng}&hourly=european_aqi'
     meteo_data = requests.get(api_request).json()
     hour = datetime.now().hour
 
     return meteo_data['hourly']['european_aqi'][hour]
+
+
+def get_air_quality_definition(index: int) -> str:
+    for range in POLLUTANT_DICT:
+        if index <= range:
+            return POLLUTANT_DICT[range]
+
+    return 'Extremely poor'
 
 
 def get_city(lat: float, lng: float) -> Worldcities:
